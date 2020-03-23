bef9ade6eba34ce944e77cb206f8566ef13e7f61
Jeremy Kale Padot <jeremy@padot.us>
2020-03-23 13:31:33 -0700

    populate controllers and models
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>
    Co-authored-by: Zakkery Short <zakkeryjshort@gmail.com>

---
+++ Registrar/Models/Student.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+namespace Registrar.Models
+{
+  public class Student
+  {
+    public Student()
+    {
+      this.Courses = new HashSet<CourseStudent>();
+    }
+    public int StudentId { get; set; }
+    public string Description { get; set; }
+
+    public ICollection<CourseStudent> Courses { get; }
+  }
+}
